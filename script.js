cce58f21ef2b59426037a8553fdc4ed054fc110a
Daniil <daniil424342@gmail.com>
2025-04-20 19:13:56 +0500

    new commit

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+import { saveTodosIntoLocalStorage, getTodosIntoLocalStorage } from "./utils.js";
 
 const addTodoInput = document.querySelector("[data-add-todo-input]");
 const addTodoBtn = document.querySelector("[data-add-todo-btn]");
+const todosContainer = document.querySelector("[data-todo-container]");
+const todoTemplate = document.querySelector("[data-todo-template]");
 
-let todoList = localStorage.getItem("todos") || [];
+let todoList = getTodosIntoLocalStorage();
 
 addTodoBtn.addEventListener("click", () => {
     if (addTodoInput.value.trim()) {
@@ -13,8 +16,34 @@ addTodoBtn.addEventListener("click", () => {
             createdAt: new Date(),
         }
         todoList.push(newTodo);
-        localStorage.setItem("todos", JSON.stringify(newTodo));
+        addTodoInput.value = "";
+        saveTodosIntoLocalStorage(todoList);
+        rendersTodos();
     }
 })
 
+const createTodoLayout = (todo) => {
+    const todoElement = document.importNode(todoTemplate.content, true);
+    const checkBox = todoElement.querySelector("[data-todo-checkbox]");
+    checkBox.checked = todo.completed;
 
+    const todoText = todoElement.querySelector("[data-todo-text]");
+    todoText.textContent = todo.text;
+
+    const todoCreatedDate = todoElement.querySelector("[data-todo-date]");
+    todoCreatedDate.textContent = todo.createdAt;
+
+    const removeTodoBtn = todoElement.querySelector("[data-remove-todo-btn]");
+    removeTodoBtn.disabled = !todo.completed;
+
+    return todoElement;
+}
+
+const rendersTodos = () => {
+    todosContainer.innerHTML = "";
+
+    todoList.forEach((todo) => {
+        const todoElement = createTodoLayout(todo);
+        todosContainer.append(todoElement);
+    })
+}
--- utils.js
+++ utils.js
@@ -1,2 +1,11 @@
 
-export
+const TODOS_KEY = "todos";
+
+export const saveTodosIntoLocalStorage = (todos) => {
+    localStorage.setItem(TODOS_KEY, JSON.stringify(todos));
+}
+
+export const getTodosIntoLocalStorage = () => {
+    return JSON.parse(localStorage.getItem(TODOS_KEY)) || [];
+}
+
